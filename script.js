aca912c395698a655f1bb8485d5b445dc9abc86f
Dipika Panda <dipikapanda029@gmail.com>
2026-06-14 20:57:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function updateData() {
+    document.getElementById("steps").innerHTML = 10000;
+    document.getElementById("calories").innerHTML = 600;
+    document.getElementById("water").innerHTML = "3 L";
+}
